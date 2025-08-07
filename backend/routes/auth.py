1b98a8fe096f534f04d4d8ffeaf4cba20ed35d7e
xenhoxi <leonjerineck@gmail.com>
2025-08-07 16:23:50 +0200

    cooking login and register

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,10 +1,16 @@
 from flask import Blueprint, render_template
+from flask import request
 
 auth_bp = Blueprint("auth", __name__)
 
-@auth_bp.route('/auth/login')
+@auth_bp.route('/auth/login', methods=['GET', 'POST'])
 def login():
-	return render_template("login.html")
+	if request.method == 'GET':
+		return render_template("login.html")
+	elif request.method == 'POST':
+		print(f"{request.form}")
+		return render_template("login.html")
+		
 
 
 @auth_bp.route('/auth/register')
